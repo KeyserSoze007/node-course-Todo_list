66f102601c17e4bc63fa21e97b4aac45f0aea32d
Satam <satam.fiem@gmail.com>
2017-11-11 17:32:34 +0530

    Adding heroku congif codes

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {ObjectID} = require('mongodb');
 
 
 var app=express();
+const port= process.env.PORT||3000;
 
 app.use(bodyParser.json());
 
@@ -52,8 +53,8 @@ app.get('/todos/:id', (req,res) => {
 });
 
 
-app.listen(3000,() => {
-    console.log('Server started on Port 3000');
+app.listen(port,() => {
+    console.log(`Server started on Port {port}`);
 });
 
 module.exports= {app};
