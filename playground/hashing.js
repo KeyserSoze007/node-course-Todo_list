890b580fe8e6267487759ed48f58bd937d85a9d6
Satam <satam.fiem@gmail.com>
2017-11-14 02:05:50 +0530

    Adding script for user password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+/*var password= '123abc';
+
+bcrypt.genSalt(10,(err,salt) => {
+    bcrypt.hash(password,salt, (err,hash) => {
+        console.log(hash);
+    });
+});*/
+
+var hashed= '$2a$10$EJXIGYocZQWBBifs8akv6uI731ylbVLFzUYG8E8Uyl/87VJ7jhmJ2';
+
+bcrypt.compare('123abc',hashed, (err,res) => {
+    console.log(res);
+});
 
 /*var msg= "This is number 4";
 
@@ -8,7 +23,7 @@ var hashing = SHA256(msg).toString();
 console.log(`The hashing of msg:${msg} is: ${hashing}`);
 */
 
-var data={
+/*var data={
     id:4
 }
 
@@ -16,4 +31,4 @@ var token=jwt.sign(data,'secret');
 console.log(token);
 
 var decoded=jwt.verify(token,'secret');
-console.log(decoded);
+console.log(decoded);*/
